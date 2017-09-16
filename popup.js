3a3c02bdb3634688b7e4abcebfcc93a6b026c303
claire <chsu.edg@gmail.com>
2017-09-16 14:08:19 -0400

    i cant believe it wanted my email in quotes

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-	document.addEventListener('DOMContentLoaded', function() {
-    		myHilitor = new Hilitor("content");
-    		myHilitor.apply("a the");
-    		//js crap
-		});
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById("button_press_on");
+    var b = document.body;
+    button.addEventListener("click",function(){
+        myHilitor = new Hilitor(b);
+        myHilitor.apply("a the");
+    });
 });
