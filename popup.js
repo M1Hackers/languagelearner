482aa59be354e4d461281f2b387c2c27ff40fab0
Aron Ricardo Perez-Lopez <arpl@mit.edu>
2017-09-17 08:32:21 -0400

    Implement search logic.

--- popup.js
+++ popup.js
@@ -1,30 +1,31 @@
-var resultSet;
+const SEARCH_LIMIT = 10;
+const CLIENT_ID = "BpQKpyTJ6W";
 
 $(document).ready(function() {
     chrome.tabs.executeScript(null,{file : 'hilitor.js'});
-    $('#submit').click(function() {
+    $('#submit_hilite').click(function() {
         setID = $('#set_id').val();
         if (setID == null || setID == "") {
             setID = 1026577;
         }
-        query(setID);
+        querySet(setID);
+    });
+    $('#submit_search').click(function() {
+        searchSets($("#search_terms").val(), SEARCH_LIMIT);
     });
 });
 
-function query(setID) {
-    var CLIENT_ID = 'BpQKpyTJ6W';
+function querySet(setID) {
     var queryURL = 'https://api.quizlet.com/2.0/sets/'+setID+'/terms?client_id='+CLIENT_ID;
 
     $.ajax({
         url: queryURL,
-        success: function(data) { onResultReceived(data) }
+        success: function(data) { onSetReceived(data) }
     });
 }
 
-function onResultReceived(data)
+function onSetReceived(resultSet)
 {
-    resultSet = data;
-
     var wordList = extractWords(resultSet);
 
     chrome.tabs.executeScript(null,{code: '(new Hilitor(document.body.innerText)).apply("'+JSON.stringify(wordList).slice(1,-1)+'");'},function(){
@@ -38,4 +39,23 @@ function extractWords(termSet) {
     }
 
     return wordString;
+}
+
+function searchSets(queryString, limit) {
+    var queryURL = 'https://api.quizlet.com/2.0/search/sets?client_id='+CLIENT_ID+'&q='+encodeURI(queryString);
+
+    $.ajax({
+        url: queryURL,
+        success: function(data) { onSetListReceived(data, limit) }
+    });
+}
+
+function onSetListReceived (resultObject, limit) {
+    setList = resultObject['sets'];
+    $("#search_results").empty();
+    var realLimit = resultObject['total_result'] < limit ? resultObject['total_result'] : limit;
+
+    for (var i = 0; i < realLimit; i++) {
+        $("#search_results").append('<input type="radio" name="wordSet" /><span>'+setList[i]['title']+' ('+setList[i]['term_count']+' terms)</span><br />');
+    }
 }
