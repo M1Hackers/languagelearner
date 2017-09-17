c9343de5237e3faaf7b8e7077386f02376685f01
Karunya Sethuraman <karunyas@mit.edu>
2017-09-17 00:45:58 -0400

    working version

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    var button = document.getElementById("button_press_on");
+    var button = document.getElementById("submit");
     button.addEventListener("click",function(){
 
       chrome.tabs.executeScript(null,{file:"hilitor.js"},function(){
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 alert(document.body.innerText);
+
 myHilitor = new Hilitor(document.body.innerText);
 myHilitor.apply('a the script');
